ff0f0c772274dc526d46ac9258dffd8c03ee949f
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-31 18:20:02 +0900

    feat: 앨범 수정 버튼 추가

--- src/(routes)/photo-album/[boardId]/_components/ModifyBtn.jsx
+++ src/(routes)/photo-album/[boardId]/_components/ModifyBtn.jsx
@@ -1,1 +1,22 @@
-export default function ModifyBtn() {}
+import { useNavigate } from 'react-router-dom';
+import { RiPencilFill } from 'react-icons/ri';
+import { CircleActivationButton as Button } from '../../../../_components/button/CircleActivationButton';
+
+/**
+ * 앨범 수정 버튼
+ * @param {boolean} existingData
+ * @returns
+ */
+export default function ModifyBtn({ existingData }) {
+    const navigate = useNavigate();
+
+    return (
+        <div className='mt-3'>
+            <Button
+                // 앨범 수정 라우팅 설정 필요
+                onClick={() => navigate(``, { state: existingData })}
+                content={<RiPencilFill className='mt-1 text-3xl' />}
+            />
+        </div>
+    );
+}
