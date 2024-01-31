7336109c8ced796d1eaec2d4ee661432d5a478d9
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-31 18:01:22 +0900

    style: 함수 선언문에서 화살표함수로 변경

--- src/_utils/StringCombinator.js
+++ src/_utils/StringCombinator.js
@@ -1,6 +1,6 @@
 export const StringCombinator = {
     // 이미지 경로를 조합하는 함수
-    getImageURL(image) {
+    getImageURL: image => {
         const imageRootURL = `${import.meta.env.VITE_APP_SERVER}/image`;
 
         return `${imageRootURL}/${image.saveFilePath}/${image.saveFileName}`;
