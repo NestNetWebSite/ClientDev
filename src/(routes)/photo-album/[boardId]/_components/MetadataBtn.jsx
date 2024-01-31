d9217dd6abd7fff659ffa05330e3fc9060f60b12
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-31 18:23:15 +0900

    feat: 앨범 메타데이터 버튼 추가

--- src/(routes)/photo-album/[boardId]/_components/MetadataBtn.jsx
+++ src/(routes)/photo-album/[boardId]/_components/MetadataBtn.jsx
@@ -1,1 +1,19 @@
-export default function MetadataBtn() {}
+import { MdComment } from 'react-icons/md';
+import { CircleActivationButton as Button } from '../../../../_components/button/CircleActivationButton';
+
+/**
+ * 앨범 메타데이터 토글 버튼
+ * @param {boolean}
+ * @returns
+ */
+export default function MetadataBtn({ setIsMetadataVisible }) {
+    const toggleMetadataVisibility = () => {
+        setIsMetadataVisible(prev => !prev);
+    };
+
+    return (
+        <div className='my-3' onClick={toggleMetadataVisibility}>
+            <Button content={<MdComment className='mt-1 text-3xl' />} />
+        </div>
+    );
+}
