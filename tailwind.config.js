764e5875a979ab5faaa790a0da188e3791b28050
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-31 15:50:14 +0900

    chore: 테일윈드 사용자 지정 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {},
+      fontSize: {},
+      colors: {
+        black: "#111111",
+        primary: "#9f1239",
+        secondary: "#be123c",
+        thinGray: "#f7f8f9",
+        thickGray: "#efefef",
+        skeleton: "#d1d5db",
+      },
+    },
   },
   plugins: [],
-}
-
+};
