b1e6481bbdb95c3f421ac45d95d7df955da796ce
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-01-31 19:54:45 +0900

    feat: 앨범 내 선택된 사진 컴포넌트 추가

--- src/(routes)/photo-album/[boardId]/_components/SelectedPhoto.jsx
+++ src/(routes)/photo-album/[boardId]/_components/SelectedPhoto.jsx
@@ -1,1 +1,44 @@
-export default function SelectedPhoto() {}
+import { useRef, memo } from 'react';
+
+/**
+ * 선택된 사진 (현재 보고 있는 사진)
+ * @param {String} selectedPhoto
+ * @returns
+ */
+export default memo(function SelectedPhoto({ selectedPhoto }) {
+    const selectedPhotoRef = useRef();
+
+    // 이미지를 전체화면으로 키우는 함수
+    const triggerImgFullScreen = element => {
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        }
+        if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        }
+        if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        }
+        if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    };
+
+    return (
+        <>
+            {selectedPhoto ? (
+                <img
+                    className='m-auto'
+                    src={selectedPhoto}
+                    alt='thumbnail'
+                    ref={selectedPhotoRef}
+                    onDoubleClick={() => triggerImgFullScreen(selectedPhotoRef.current)}
+                />
+            ) : (
+                <>
+                    <div className='h-full w-full animate-pulse bg-skeleton' />
+                </>
+            )}
+        </>
+    );
+});
