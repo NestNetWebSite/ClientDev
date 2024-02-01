df81e47c2701492b94325c153dac9a0983e21865
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-02 03:34:42 +0900

    feat: 공지사항 게시글 작성 페이지 추가

---
+++ src/(routes)/notice/post/page.tsx
@@ -0,0 +1,9 @@
+import NoticeBoardPostForm from './_component/NoticeBoardPostForm';
+
+export default function Page() {
+    return (
+        <main className={'w-full'}>
+            <NoticeBoardPostForm />
+        </main>
+    );
+}
