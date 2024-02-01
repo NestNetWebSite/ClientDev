b09f542106652e76b8e9648bca23395fc51b8b76
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-02 03:38:52 +0900

    feat: 공지사항 페이지 관련 타입 추가

---
+++ src/(routes)/notice/types.ts
@@ -0,0 +1,26 @@
+export interface FileData {
+    id: number;
+    originalFileName: string;
+    saveFileName: string;
+}
+
+export interface CommentData {
+    id: number;
+    username: string;
+    content: string;
+    createdTime: number[];
+    modifiedTime: number[] | null;
+    memberWritten: boolean;
+}
+
+export interface NoticeBoardData {
+    id: number;
+    title: string;
+    bodyContent: string;
+    viewCount: number;
+    likeCount: number;
+    username: string;
+    createdTime: string;
+    modifiedTime: string | null;
+    memberWritten: boolean;
+}
