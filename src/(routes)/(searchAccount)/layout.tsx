682f6749b9b91e3e3810abfac1940df3cbfb7423
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-02 03:26:07 +0900

    feat: 아이디, 비밀번호 찾기 페이지 추가

---
+++ src/(routes)/(searchAccount)/layout.tsx
@@ -0,0 +1,28 @@
+import { Link, Outlet } from 'react-router-dom';
+import SideNavbar from './_component/SideNavbar';
+
+export default function Layout() {
+    return (
+        <div className={'mx-auto flex w-[32rem] flex-col items-center'}>
+            <header className={'mb-9 mt-14'}>
+                <Link className={'text-5xl font-bold tracking-wider text-rose-700'} to={'/'}>
+                    Nest.Net
+                </Link>
+            </header>
+            <div className={'w-full rounded-3xl border border-gray-300 bg-white'}>
+                <SideNavbar />
+                <div className={'p-8'}>
+                    <Outlet />
+                    <div className={'w-full text-end'}>
+                        <Link
+                            className={'text-[0.9rem] font-bold text-gray-500 transition-all hover:text-rose-700'}
+                            to={'signin'}
+                        >
+                            로그인 / 회원가입
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
