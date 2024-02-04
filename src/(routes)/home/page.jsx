a4467db8491aa424d7a2a2d2b8e380d31864de8a
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-02-04 16:22:29 +0900

    feat: 메인 홈 페이지 추가

---
+++ src/(routes)/home/page.jsx
@@ -0,0 +1,39 @@
+import { useInView } from 'react-intersection-observer';
+import MainView from './main_view/page';
+import PhotoZone from './photo_zone/page';
+import Services from './services/page';
+import Footer from './footer/page';
+
+/**
+ * 메인 홈
+ * @returns
+ */
+export default function Page() {
+    // api 호출 위한 포토존 영역 옵저버
+    const { ref: photoZoneRef, inView } = useInView({
+        triggerOnce: true,
+    });
+
+    return (
+        <>
+            {/* 메인뷰 */}
+            <section>
+                <MainView />
+            </section>
+            {/* 고정 배경화면 */}
+            <section className="h-[9rem] bg-[url('./_assets/images/nestnet-bg.png')]  bg-fixed bg-bottom bg-no-repeat"></section>
+            {/* 포토존 */}
+            <section className='relative -top-44' ref={photoZoneRef}>
+                <PhotoZone inView={inView} />
+            </section>
+            {/* 서비스 */}
+            <section className='px-14 md:px-20 xl:px-24'>
+                <Services />
+            </section>
+            {/* 푸터 */}
+            <section>
+                <Footer />
+            </section>
+        </>
+    );
+}
