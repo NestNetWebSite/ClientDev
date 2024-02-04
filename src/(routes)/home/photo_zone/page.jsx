04b5b797e20e8c6a085eb563ee2069e2302a3a29
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-02-04 15:56:06 +0900

    feat: 메인 포토존 영역 페이지 추가

---
+++ src/(routes)/home/photo_zone/page.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import SlidingPhotos from './_components/SlidingPhotos';
+import PhotoPostForm from './_components/PhotoPostForm';
+
+/**
+ * 포토존(인생네컷) 섹션
+ * @param {boolean}
+ * @returns
+ */
+export default function Page({ inView }) {
+    // 옵저버 감지시 조회 api 호출
+    const { data: photos = [], isLoading: isPhotosLoading } = useGetPhotos(inView);
+
+    return (
+        <div className='pt-64 dark:bg-gray-900'>
+            {/* 사진 업로드 */}
+            <div className='header mb-14 flex flex-row justify-start pl-16'>
+                <PhotoPostForm />
+            </div>
+            {/* 사진 리스트 */}
+            {isPhotosLoading ? (
+                // LOADING: 스켈레톤
+                <div className='flex h-[30rem] w-full flex-row overflow-hidden'>
+                    {Array.from(new Array(5)).map((_, idx) => (
+                        <div
+                            key={idx}
+                            className='mx-2 h-[28rem] w-[20rem] min-w-[20rem] animate-pulse rounded-sm bg-skeleton'
+                        />
+                    ))}
+                </div>
+            ) : (
+                <SlidingPhotos photos={photos} />
+            )}
+        </div>
+    );
+}
+
+// REST: 포토존 사진 조회
+const useGetPhotos = inView => {
+    return useQuery({
+        queryKey: ['photo-zone'],
+        queryFn: async () => {
+            const photoZoneURL = `${import.meta.env.VITE_APP_SERVER}/life4cut?size=20`;
+            return await axios.get(photoZoneURL).then(res => {
+                return res.data.response.dtoList;
+            });
+        },
+        gcTime: 0,
+        enabled: inView,
+    });
+};
