97ecca3221f3356cae8328f9812ab6c9a30ab4c5
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-07 04:09:39 +0900

    feat: 현 임원 관리 페이지 추가 (미완성)

---
+++ src/(routes)/admin/executives/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className={'pl-[14rem] pt-20'}>
+            <h3 className={'px-11 py-10 text-lg font-bold'}>현 임원 관리</h3>
+        </div>
+    );
+}
