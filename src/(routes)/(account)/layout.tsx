8be8397e050a8e47ff041a703afc2c54db7853b3
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-07 03:50:46 +0900

    design: 로그인, 회원가입 페이지 디자인 수정

--- src/(routes)/(account)/layout.tsx
+++ src/(routes)/(account)/layout.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from 'react-router-dom';
+import LOGO_URL from '/public/_assets/images/nestnet-logo.png';
 import SideNavbar from './_component/SideNavbar';
 
 export default function Layout() {
@@ -6,10 +7,10 @@ export default function Layout() {
         <div className={'mx-auto flex w-[32rem] flex-col items-center'}>
             <header className={'mb-9 mt-14'}>
                 <Link className={'text-5xl font-bold tracking-wider text-rose-700'} to={'/'}>
-                    Nest.Net
+                    <img className={'w-[18rem]'} src={LOGO_URL} alt={'NestNetLogo'} />
                 </Link>
             </header>
-            <div className={'mb-14 w-full rounded-3xl border border-gray-300 bg-white'}>
+            <div className={'mb-14 w-full rounded-2xl border border-gray-300 bg-white'}>
                 <SideNavbar />
                 <Outlet />
             </div>
