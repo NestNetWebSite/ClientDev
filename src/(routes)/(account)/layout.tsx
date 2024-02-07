bcab0983efe42e0369ed29c236950cb3834c7bd5
Kim Kangmin <rlarkdals1202@gmail.com>
2024-02-08 01:29:00 +0900

    fix: 이미지 경로 수정

--- src/(routes)/(account)/layout.tsx
+++ src/(routes)/(account)/layout.tsx
@@ -1,5 +1,4 @@
 import { Link, Outlet } from 'react-router-dom';
-import LOGO_URL from '/public/_assets/images/nestnet-logo.png';
 import SideNavbar from './_component/SideNavbar';
 
 export default function Layout() {
@@ -7,7 +6,7 @@ export default function Layout() {
         <div className={'mx-auto flex w-[32rem] flex-col items-center'}>
             <header className={'mb-9 mt-14'}>
                 <Link className={'text-5xl font-bold tracking-wider text-rose-700'} to={'/'}>
-                    <img className={'w-[18rem]'} src={LOGO_URL} alt={'NestNetLogo'} />
+                    <img className={'w-[18rem]'} src={'/_assets/images/nestnet-logo.png'} alt={'NestNetLogo'} />
                 </Link>
             </header>
             <div className={'mb-14 w-full rounded-2xl border border-gray-300 bg-white'}>
