6bfb889e5e50029096733cdd96072ec93d6d11a0
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-02 19:06:43 +0900

    feat: 현 임원 조회 기능 추가

--- src/(routes)/admin/executives/page.tsx
+++ src/(routes)/admin/executives/page.tsx
@@ -1,7 +1,10 @@
+import ExecutivesTable from '../_components/ExecutivesTable.tsx';
+
 export default function Page() {
     return (
         <div className={'pl-[14rem] pt-20'}>
-            <h3 className={'px-11 py-10 text-lg font-bold'}>현 임원 관리</h3>
+            <h1 className={'px-11 py-10 text-lg font-bold'}>현 임원 관리</h1>
+            <ExecutivesTable />
         </div>
     );
 }
