d01deccbd74a9f31bf385f357070a14827ec5bd3
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-03-14 23:40:43 +0900

    refactor: 메인페이지 출석 버튼 js -> ts

--- src/(routes)/home/main_view/_components/AttendanceBtn.tsx
+++ src/(routes)/home/main_view/_components/AttendanceBtn.tsx
@@ -1,25 +1,19 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { FaCalendar } from 'react-icons/fa';
-import { FaCalendarCheck } from 'react-icons/fa';
 
-/**
- * 출석 버튼
- * @param {boolean}
- * @returns
- */
 export default function AttendanceBtn() {
     const { mutate: createMyAttd, isPending: isMyAttdPending } = usePostMyAttendance();
 
     return (
         <div className='absolute bottom-0 right-0 h-[4rem] w-[4rem] overflow-hidden'>
-            <div
+            <button
                 className='flex h-full w-full cursor-pointer flex-col items-center justify-center rounded-full border-2 border-secondary bg-white text-primary transition-all hover:scale-90 hover:text-secondary'
                 onClick={() => createMyAttd()}
                 disabled={isMyAttdPending}
             >
                 <FaCalendar className='hover:primary text-3xl ' />
-            </div>
+            </button>
         </div>
     );
 }
@@ -28,18 +22,20 @@ export default function AttendanceBtn() {
 function usePostMyAttendance() {
     const queryClient = useQueryClient();
 
-    return useMutation({
+    return useMutation<AxiosResponse>({
         mutationFn: async () => {
             const myAttdURL = `/api/attendance`;
             return await axios.post(myAttdURL);
         },
         // 클라이언트 업데이트
         onSuccess: () => {
-            window.alert('출석 완료!');
-            queryClient.invalidateQueries(['attendance-statistics']);
+            alert('출석 완료!');
+            queryClient.invalidateQueries({ queryKey: ['attendance-statistics'] });
         },
         onError: async error => {
-            window.alert(error.response.data.error.message);
+            // @ts-ignore
+            // 타입 지정 필요
+            alert(error?.response.data.error.message);
         },
         retry: 0,
     });
