14652ccbd57f8bb13fc2f25c87c3ed7a5c1d9338
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-03-14 22:58:16 +0900

    refactor:  관리자 메인 페이지 및 네비게이션바 js -> ts

--- src/(routes)/admin/page.tsx
+++ src/(routes)/admin/page.tsx
@@ -1,10 +1,7 @@
 import NavigationBar from './_components/NavigationBar';
 import { Outlet } from 'react-router-dom';
 
-/**
- * 관리자 페이지
- * @returns
- */
+// 관리자 페이지
 export default function Page() {
     return (
         <>
