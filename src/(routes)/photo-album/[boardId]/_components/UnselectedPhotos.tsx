fe67e33234ecac76a4136ae55b7f897c825d9d44
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-03-15 17:09:48 +0900

    refactor: 사진게시판 단건 페이지 선택되지 않은 사진들  js -> ts

--- src/(routes)/photo-album/[boardId]/_components/UnselectedPhotos.tsx
+++ src/(routes)/photo-album/[boardId]/_components/UnselectedPhotos.tsx
@@ -1,20 +1,21 @@
-import { memo } from 'react';
+import { SetStateAction, memo, Dispatch } from 'react';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 import { StringCombinator } from '../../../../_utils/StringCombinator';
+import { IExistingFileDto } from '../../types';
 
-/**
- * 선택되지 않은 사진들
- * @param {boolean, Object[], setState}
- * @returns
- */
-export default memo(function UnselectedPhotos({ isAlbumLoading, photos, setSelectedPhoto }) {
+interface IProps {
+    photos: IExistingFileDto[];
+    setSelectedPhoto: Dispatch<SetStateAction<string>>;
+}
+
+export default memo(function UnselectedPhotos({ photos, setSelectedPhoto }: IProps) {
     // 클릭된 사진으로 선택된 사진 변경하는 핸들러
-    const handleUnselectedPhotoClick = photoPath => {
+    const handleUnselectedPhotoClick = (photoPath: string) => {
         setSelectedPhoto(photoPath);
     };
 
     // LOADING: 스켈레톤 효과 표시
-    if (isAlbumLoading) {
+    if (!photos) {
         return (
             <div className='flex h-screen w-full flex-col items-center justify-between'>
                 {Array.from(new Array(4)).map((_, idx) => (
@@ -24,22 +25,26 @@ export default memo(function UnselectedPhotos({ isAlbumLoading, photos, setSelec
         );
     }
 
-    if (!isAlbumLoading) {
+    if (photos) {
         return (
             <>
-                {photos?.map(photo => (
+                {photos.map(photo => (
                     <div
                         key={photo.id}
                         className='ImageWrapper relative mb-4 overflow-hidden rounded-lg shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] hover:shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]'
                     >
                         <img
                             className='w-full'
-                            src={StringCombinator.getImageURL(photo)}
+                            src={StringCombinator.getImageURL(photo.saveFilePath, photo.saveFileName)}
                             alt={photo.originalFileName}
                         />
                         <div
                             className='ImageCover absolute left-0 top-0 flex h-full w-full flex-col items-center justify-center bg-black text-center text-sm text-white opacity-0 duration-300 ease-in-out hover:cursor-pointer hover:opacity-75'
-                            onClick={() => handleUnselectedPhotoClick(StringCombinator.getImageURL(photo))}
+                            onClick={() =>
+                                handleUnselectedPhotoClick(
+                                    StringCombinator.getImageURL(photo.saveFilePath, photo.saveFileName),
+                                )
+                            }
                         >
                             <FaMagnifyingGlass className={'text-4xl'} />
                         </div>
