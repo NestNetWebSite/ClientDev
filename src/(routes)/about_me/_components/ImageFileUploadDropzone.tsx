d9eb7fba3dc53909899344204424679da92d686e
Kim Kangmin <rlarkdals1202@gmail.com>
2024-03-27 23:56:00 +0900

    refactor: tailwind css 클래스 명 수정
    자기소개 페이지

--- src/(routes)/about_me/_components/ImageFileUploadDropzone.tsx
+++ src/(routes)/about_me/_components/ImageFileUploadDropzone.tsx
@@ -31,19 +31,19 @@ export default function ImageFileUploadDropzone({ addImageFile }: Props) {
             <span className={'mx-2.5 mb-2 text-sm text-red-500'}>
                 .jpg, .jpeg, .png 파일만 첨부할 수 있습니다. 다른 확장자의 파일을 선택하면, 첨부되지 않습니다.
             </span>
-            <div className={'relative h-64 rounded-xl border border-dashed border-rose-700'} {...getRootProps()}>
+            <div className={'relative h-64 rounded-xl border border-dashed border-secondary'} {...getRootProps()}>
                 <input {...getInputProps()} />
                 <div
                     className={
                         'absolute left-1/2 top-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-y-3'
                     }
                 >
-                    <MdOutlineFileUpload className={'h-12 w-12 text-rose-700'} />
+                    <MdOutlineFileUpload className={'h-12 w-12 text-secondary'} />
                     <span className={'font-semibold'}>드로그 앤 드롭으로 파일 추가</span>
                     <span className={'text-sm text-gray-600'}>또는</span>
                     <button
                         type={'button'}
-                        className={'rounded-2xl bg-rose-700 px-3 py-2 text-white transition-all hover:bg-rose-900'}
+                        className={'rounded-2xl bg-secondary px-3 py-2 text-white transition-all hover:bg-primary'}
                         onClick={open}
                     >
                         파일 탐색
