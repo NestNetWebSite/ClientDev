52c12935c5d902b77498734af402f4fda591ce5b
wonilheo <70367717+wonil-programmer@users.noreply.github.com>
2024-08-26 00:22:43 +0900

    refactor: 출석 등록 버튼 api 분리

--- src/(routes)/home/main_view/_components/AttendanceBtn.tsx
+++ src/(routes)/home/main_view/_components/AttendanceBtn.tsx
@@ -1,8 +1,7 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import axios, { AxiosError, AxiosResponse } from 'axios';
 import { FaCalendar } from 'react-icons/fa';
-import isServerError from '../../../../_errors/isServerError';
+import { usePostMyAttendance } from '../../../../api/home-api';
 
+// COMPONENT: 출석체크 버튼
 export default function AttendanceBtn() {
     const { mutate: createMyAttd, isPending: isMyAttdPending } = usePostMyAttendance();
 
@@ -22,41 +21,4 @@ export default function AttendanceBtn() {
             </div>
         </button>
     );
-}
-
-// REST: 출석 등록
-function usePostMyAttendance() {
-    const queryClient = useQueryClient();
-
-    return useMutation<AxiosResponse, AxiosError>({
-        mutationFn: async () => {
-            const myAttdURL = `/api/attendance`;
-            return await axios.post(myAttdURL);
-        },
-        // 클라이언트 업데이트
-        onSuccess: () => {
-            alert('출석 완료!');
-            queryClient.invalidateQueries({ queryKey: ['attendance-statistics'] });
-        },
-        onError: async e => {
-            let errorMessage = '';
-            if (isServerError(e) && e.response.data && e?.response?.data?.error.message) {
-                errorMessage = e.response.data.error.message;
-                alert(errorMessage);
-
-                return;
-            }
-            if (e.response.status === 403) {
-                errorMessage = '권한이 없는 사용자입니다';
-            } else if (e.response.status === 401) {
-                errorMessage = '로그인 후 다시 시도해주세요.';
-            } else if (e.response.status === 404) {
-                errorMessage = '오늘은 이미 출석하셨습니다! 내일도 방문해주세요!';
-            } else if (e.response.status === 500) {
-                errorMessage = '출석 등록에 실패하였습니다. 관리자에게 문의해주세요.';
-            }
-            alert(errorMessage);
-        },
-        retry: 0,
-    });
 }
--- src/api/home-api.ts
+++ src/api/home-api.ts
@@ -1,6 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { IAttdRanks, INewPost } from '../(routes)/home/type';
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import isServerError from '../_errors/isServerError';
 
 // REST: 최근글 조회
 export const useGetNewPosts = () => {
@@ -30,3 +31,40 @@ export const useGetAttendance = () => {
         retry: 0,
     });
 };
+
+// REST: 출석 등록
+export function usePostMyAttendance() {
+    const queryClient = useQueryClient();
+
+    return useMutation<AxiosResponse, AxiosError>({
+        mutationFn: async () => {
+            const myAttdURL = `/api/attendance`;
+            return await axios.post(myAttdURL);
+        },
+        // 클라이언트 업데이트
+        onSuccess: () => {
+            alert('출석 완료!');
+            queryClient.invalidateQueries({ queryKey: ['attendance-statistics'] });
+        },
+        onError: async e => {
+            let errorMessage = '';
+            if (isServerError(e) && e.response.data && e?.response?.data?.error.message) {
+                errorMessage = e.response.data.error.message;
+                alert(errorMessage);
+
+                return;
+            }
+            if (e.response.status === 403) {
+                errorMessage = '권한이 없는 사용자입니다';
+            } else if (e.response.status === 401) {
+                errorMessage = '로그인 후 다시 시도해주세요.';
+            } else if (e.response.status === 404) {
+                errorMessage = '오늘은 이미 출석하셨습니다! 내일도 방문해주세요!';
+            } else if (e.response.status === 500) {
+                errorMessage = '출석 등록에 실패하였습니다. 관리자에게 문의해주세요.';
+            }
+            alert(errorMessage);
+        },
+        retry: 0,
+    });
+}
